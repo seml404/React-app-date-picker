3876742e851ab9a10b356b12797de147519f1f78
SEML <4040591@gmail.com>
2021-12-28 14:44:18 +0300

    second  commit; (1) refactored code for divs and not HTML table; (2) added styles;

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -11,32 +11,42 @@ function App() {
   }
   console.log(calendar);
   let weekCounter = calendar.length / 7;
-  let weeksArr = [];
-  for (let i = 0; i < weekCounter; i++) {
-    weeksArr.push(i);
-  }
 
-  function renderTds(arr, start) {
-    let arrOfTds = [];
+  function renderDays(arr, start) {
+    let arrOfDays = [];
     for (let i = start, j = 0; j < 7; j++, i++) {
-      arrOfTds.push(<td key={arr[i]}>{arr[i]}</td>);
+      arrOfDays.push(
+        <div className="DayPicker-Day" key={arr[i]}>
+          <div>
+            <div>{arr[i]}</div>
+          </div>
+        </div>
+      );
     }
-    return arrOfTds;
+    return arrOfDays;
   }
 
   function renderWeeks(arr) {
     let arrOfWeeks = [];
     for (let j = 0, days = 0; j < weekCounter; j++, days += 7) {
-      arrOfWeeks.push(<tr key={j}>{renderTds(arr, days)}</tr>);
+      arrOfWeeks.push(
+        <div className="DayPicker-Week" key={j}>
+          {renderDays(arr, days)}
+        </div>
+      );
     }
     return arrOfWeeks;
   }
 
   return (
     <>
-      <table>
-        <tbody>{renderWeeks(calendar)}</tbody>
-      </table>
+      <div className="DayPicker-Months">
+        <div className="DayPicker-Month">
+          <div className="DayPicker-Weekdays"></div>
+          <div className="DayPicker-Caption">Декабрь 2021</div>
+          <div className="DayPicker-Body">{renderWeeks(calendar)}</div>
+        </div>
+      </div>
     </>
   );
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
 
-// let currentDate = DateTime.now();
+let currentDate = DateTime.now();
 
-const currentDate = DateTime.local(2021, 9, 15, 8, 30);
+// const currentDate = DateTime.local(2021, 9, 15, 8, 30);
 
 export { DateTime, currentDate };
